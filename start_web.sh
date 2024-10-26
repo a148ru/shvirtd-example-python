21e27951cb2f4c414536c5a44802f1a25306f099
Anton <ant.band@mail.ru>
2024-10-26 20:37:29 +0300

    2

--- start_web.sh
+++ start_web.sh
@@ -3,15 +3,15 @@
 echo "waitin 10 seconds db"
 sleep 5
 
-HOST=localhost
+HOST=172.20.0.10
 PORT=3306
 
 
-#count=0
-while [ -z $(echo "" > /dev/tcp/$HOST/$PORT && echo "Opened" || echo "Closed" | grep Opened) ] #&& [ $count != 10 ]
+count=0
+while [ -z $(echo "" > /dev/tcp/$HOST/$PORT && echo "Opened" || echo "Closed" | grep Opened) ] && [ $count != 10 ]
 do
 sleep 5
-#count=$((count+1))
+count=$((count+1))
 done
 
 # run app
